b4f3f74205a7fcbd32abc0db7c708825233fbf86
Phil Booth <pmbooth@gmail.com>
2018-05-02 17:25:23 +0100

    refactor(settings): extract types for deserialization
    Extracts the inline validation code from the `settings` module to a
    dedicated `deserialize` module, so that the validation rules can be
    better re-used elsewhere.
    
    Relies on a currently-unmerged PR in order to work, hence the `config`
    crate is now resolved with a git URL rather than a version string.
    There is an equivalent refactoring that doesn't require this fix, which
    uses functions and serde's `deserialize_with` macro instead of newtype
    structs.
    
    The other gotcha was that I had to flatten the `Settings` struct in
    order for the tests to be able to create clean instances with fresh
    values. This is a little bit annoying and is also not necessary for the
    alternative, function-based refactoring. I did spend a bit of time
    trying to dig into the root cause, without success. I'll open an issue
    in the appropriate repo as soon as I have a minimal test case, or come
    back and fix if it's something wrong here, which is eminently possible.

--- src/settings/test.rs
+++ src/settings/test.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use super::*;
+use deserialize::{Host, Sender};
 
 lazy_static! {
   // HACK: mutex to prevent tests clobbering each other's environment variables
@@ -65,65 +66,71 @@ impl<'e> Drop for CleanEnvironment<'e>
 fn env_vars_take_precedence()
 {
   let _clean_env = CleanEnvironment::new(vec![
-    "FXA_SMTP_HOST",
-    "FXA_SMTP_PORT",
-    "FXA_SMTP_SENDER",
-    "FXA_SMTP_USER",
-    "FXA_SMTP_PASSWORD",
+    "FXA_EMAIL_SENDER",
+    "FXA_EMAIL_SMTP_HOST",
+    "FXA_EMAIL_SMTP_PORT",
+    "FXA_EMAIL_SMTP_USER",
+    "FXA_EMAIL_SMTP_PASSWORD",
   ]);
 
   match Settings::new() {
     Ok(settings) => {
-      let host = format!("{}{}", settings.smtp.host, "1");
-      let port = settings.smtp.port + 2;
-      let sender = format!("{}{}", "3", settings.smtp.sender);
-      let user = if let Some(ref user) = settings.smtp.user {
+      let sender = Sender(format!("{}{}", "1", settings.sender.as_str()));
+      let smtp_host = Host(format!("{}{}", settings.smtp_host.as_str(), "2"));
+      let smtp_port = settings.smtp_port + 3;
+      let smtp_user = if let Some(ref user) = settings.smtp_user {
         format!("{}{}", user, "4")
       } else {
         String::from("4")
       };
-      let password = if let Some(ref password) = settings.smtp.password {
+      let smtp_password = if let Some(ref password) = settings.smtp_password {
         format!("{}{}", password, "5")
       } else {
         String::from("5")
       };
 
-      env::set_var("FXA_SMTP_HOST", &host);
-      env::set_var("FXA_SMTP_PORT", &port.to_string());
-      env::set_var("FXA_SMTP_SENDER", &sender);
-      env::set_var("FXA_SMTP_USER", &user);
-      env::set_var("FXA_SMTP_PASSWORD", &password);
+      env::set_var("FXA_EMAIL_SENDER", sender.as_str());
+      env::set_var("FXA_EMAIL_SMTP_HOST", smtp_host.as_str());
+      env::set_var("FXA_EMAIL_SMTP_PORT", &smtp_port.to_string());
+      env::set_var("FXA_EMAIL_SMTP_USER", &smtp_user);
+      env::set_var("FXA_EMAIL_SMTP_PASSWORD", &smtp_password);
 
       match Settings::new() {
         Ok(env_settings) => {
-          assert_eq!(env_settings.smtp.host, host);
-          assert_eq!(env_settings.smtp.port, port);
-          assert_eq!(env_settings.smtp.sender, sender);
+          assert_eq!(env_settings.sender, sender);
+          assert_eq!(env_settings.smtp_host, smtp_host);
+          assert_eq!(env_settings.smtp_port, smtp_port);
 
-          if let Some(env_user) = env_settings.smtp.user {
-            assert_eq!(env_user, user);
+          if let Some(env_user) = env_settings.smtp_user {
+            assert_eq!(env_user, smtp_user);
           } else {
-            assert!(false, "smtp.user was not set");
+            assert!(false, "smtp_user was not set");
           }
 
-          if let Some(env_password) = env_settings.smtp.password {
-            assert_eq!(env_password, password);
+          if let Some(env_password) = env_settings.smtp_password {
+            assert_eq!(env_password, smtp_password);
           } else {
-            assert!(false, "smtp.password was not set");
+            assert!(false, "smtp_password was not set");
           }
         }
-        Err(error) => assert!(false, error),
+        Err(error) => {
+          println!("{}", error);
+          assert!(false);
+        }
       }
     }
-    Err(error) => assert!(false, error),
+    Err(error) => {
+      println!("{}", error);
+      assert!(false);
+    }
   }
 }
 
 #[test]
 fn invalid_host()
 {
-  let _clean_env = CleanEnvironment::new(vec!["FXA_SMTP_HOST"]);
-  env::set_var("FXA_SMTP_HOST", "https://mail.google.com/");
+  let _clean_env = CleanEnvironment::new(vec!["FXA_EMAIL_SMTP_HOST"]);
+  env::set_var("FXA_EMAIL_SMTP_HOST", "https://mail.google.com/");
 
   match Settings::new() {
     Ok(_settings) => assert!(false, "Settings::new should have failed"),
@@ -134,8 +141,8 @@ fn invalid_host()
 #[test]
 fn invalid_sender()
 {
-  let _clean_env = CleanEnvironment::new(vec!["FXA_SMTP_SENDER"]);
-  env::set_var("FXA_SMTP_SENDER", "wibble");
+  let _clean_env = CleanEnvironment::new(vec!["FXA_EMAIL_SENDER"]);
+  env::set_var("FXA_EMAIL_SENDER", "wibble");
 
   match Settings::new() {
     Ok(_settings) => assert!(false, "Settings::new should have failed"),
