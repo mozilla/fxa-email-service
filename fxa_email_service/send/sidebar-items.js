8a19b07306a288da122bc4d7d82536df5a315ccb
fxa-devs <fxa-core@mozilla.com>
2018-07-12 17:02:53 +0000

    chore(docs): rebuild developer docs

--- fxa_email_service/send/sidebar-items.js
+++ fxa_email_service/send/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"static":[["static_rocket_route_info_for_handler","Rocket code generated static route information structure."]]});
+initSidebarItems({"constant":[["_IMPL_DESERIALIZE_FOR_Body",""],["_IMPL_DESERIALIZE_FOR_Email",""]],"fn":[["handler",""],["rocket_route_fn_handler",""],["validate",""]],"static":[["static_rocket_route_info_for_handler","Rocket code generated static route information structure."]],"struct":[["Body",""],["Email",""]]});
---
+++ implementors/rocket/data/from_data/trait.FromData.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fxa_email_service"] = [{text:"impl <a class=\"trait\" href=\"https://api.rocket.rs/rocket/data/from_data/trait.FromData.html\" title=\"trait rocket::data::from_data::FromData\">FromData</a> for <a class=\"struct\" href=\"fxa_email_service/send/struct.Email.html\" title=\"struct fxa_email_service::send::Email\">Email</a>",synthetic:false,types:[]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
